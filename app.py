158590af90d6bfe43f2690bdff3452e6546378bc
Parag Jhunjhunwala <87004753+paragjjw@users.noreply.github.com>
2023-03-31 02:48:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,9 +6,22 @@ from PIL import Image
 import numpy as np
 import re
 from io import BytesIO
+import base64
+import streamlit.components.v1 as components
 st.set_page_config(page_title='Movie Recommendation System', layout='wide')
-mood_dict = {'Happy': 'Horror', 'Sad': 'Drama', 'Satisfied': 'Animation', 'Angry': 'Romance', 'Peaceful': 'Fantasy',
-             'Fearful': 'Adventure', 'Excited': 'Crime', 'Depressed': 'Comedy', 'Content': 'Mystery', 'Sorrowful': 'Action'}
+# mood_dict = {'Happy': 'Horror', 'Sad': 'Drama', 'Satisfied': 'Animation', 'Angry': 'Romance', 'Peaceful': 'Fantasy',
+#              'Fearful': 'Adventure', 'Excited': 'Crime', 'Depressed': 'Comedy', 'Content': 'Mystery', 'Sorrowful': 'Action'}
+
+
+def local_css(file_name):
+    html_string = ''
+    with open(file_name) as f:
+        html_string = html_string+f.read()
+    return html_string
+
+
+def icon(icon_name):
+    return f'<i class="material-icons">{icon_name}</i>'
 
 
 def generate_set(attribute):
@@ -26,14 +39,98 @@ def generate_set(attribute):
 def fetch_poster(index):
     path = movies.iloc[index].poster_path
     if(path != path):
-        path = 'poster.png'
+        path = "data:image/png;base64,{0}".format(
+            base64.b64encode(open('poster.png', 'rb').read()).decode('utf-8'))
     else:
         response = requests.get(path)
         if(response.status_code != 200):
-            path = 'poster.png'
-        else:
-            path = BytesIO(response.content)
-    return Image.open(path).resize((400, 600))
+            path = "data:image/png;base64,{0}".format(
+                base64.b64encode(open('poster.png', 'rb').read()).decode('utf-8'))
+    #     else:
+    #         path = BytesIO(response.content)
+    # return Image.open(path).resize((400, 600))
+    return path
+
+
+def get_year(index):
+    html_string = ''
+    if(movies.iloc[index].year_of_release == movies.iloc[index].year_of_release):
+        html_string += '<h6>' + \
+            str(int(movies.iloc[index].year_of_release))+'</h6>'
+    return html_string
+
+
+def get_runtime(index):
+    html_string = ''
+    if(movies.iloc[index].runtime == movies.iloc[index].runtime):
+        html_string += '<div style="display:inline-flex">'+icon('timer')+'<h6>' + \
+            str(int(movies.iloc[index].runtime))+'min</h6></div>'
+    return html_string
+
+
+def get_rating(index):
+    html_string = ''
+    if(movies.iloc[index].imdb_rating == movies.iloc[index].imdb_rating):
+        html_string += '<div style="display:inline-flex">'+icon('star')+'<h6>' + \
+            str(int(movies.iloc[index].imdb_rating))+'/10</h6></div>'
+    return html_string
+
+
+def get_story(index):
+    html_string = ''
+    if(movies.iloc[index].story == movies.iloc[index].story):
+        html_string += '<p>' + \
+            movies.iloc[index].story[0:min(
+                len(movies.iloc[index].story), 200)]
+        if(html_string[-1] != '.'):
+            html_string += "..."
+        html_string += '</p>'
+    return html_string
+
+
+def get_genres(index):
+    html_string = ''
+    for genre in movies.iloc[index].genres:
+        html_string += '<div class="genre">' + \
+            genre+'</div>'
+    return html_string
+
+
+def display_poster(index_list):
+    html_string = """
+    <style>{style}</style>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <div class="recommendations">
+    """.format(style=local_css("style.css"))
+    for i in index_list:
+        html_string += """<div class = "card" >
+    <div class = "card-header" style = "position:relative" >
+    <img src = "{img_url}" alt = "image not found" style = "width:100%; height:350px" >
+    <div class = "movie_details" >
+    <h2 class = "title" > {title} </h2>
+    <div class = "year_runtime">
+    {year}
+    {runtime}
+    {rating}
+    </div>
+    <div class = "genres">
+    {genres}
+    </div >
+    <div class = "story">
+    {story}
+    </div >
+    <div class = "more_info">
+    <a href = "{wiki_link}" target = "_blank"> <button type = "button" class = "btn"> Read More </button> </a>
+    </div>
+    </div>
+    </div>
+<div class = "card-body" >
+    <h5 class = "card-title" > {title} </h5>
+  </div>  
+</div>
+    """.format(wiki_link=movies.iloc[i].wiki_link, story=get_story(i), genres=get_genres(i), rating=get_rating(i), year=get_year(i), runtime=get_runtime(i), img_url=fetch_poster(i), title=movies.iloc[i].title)
+    html_string += "</div>"
+    components.html(html_string, height=2500, scrolling=True)
 
 
 def recommend_from_name(movie, adult):
@@ -46,10 +143,9 @@ def recommend_from_name(movie, adult):
     return sorted_list
 
 
-def recommend_from_mood(genre, adult):
-    sorted_list = sorted([x for x in range(len(list(movies['genres']))) if genre in movies.iloc[x].genres and (
-        adult or not movies.iloc[x].adult)], key=lambda x: movies.iloc[x].imdb_rating, reverse=True)[0:25]
-    return sorted_list
+def recommend_from_mood(emotion, adult):
+    return sorted([x for x in movies.index if movies.iloc[x].story == movies.iloc[x].story],
+                  key=lambda x: movies.iloc[x].Emotion[0][emotion], reverse=True)[0:25]
 
 
 def recommend_from_details(details):
@@ -71,6 +167,7 @@ def recommend_from_details(details):
         #     take = False
         # if movies['release_date'][i] < details['release_date']:
         #     take = False
+        # print(details['cast'])
         if not all(cast in movies['actors'][i] for cast in details['cast']):
             take = False
         if not all(director in movies['directors'][i] for director in details['director']):
@@ -84,17 +181,6 @@ def recommend_from_details(details):
     if(len(sorted_list) > 25):
         sorted_list = sorted_list[0:25]
     return sorted_list
-
-
-def display_poster(index_list):
-    for i in range(0, len(index_list), 5):
-        with st.container():
-            cols = st.columns(5)
-            for j in range(0, 5):
-                if(i+j < len(index_list)):
-                    with cols[j]:
-                        st.image(fetch_poster(
-                            index_list[i+j]), caption=movies.iloc[index_list[i+j]].title)
 
 
 similarity = pickle.load(open('similarity_2.pkl', 'rb'))
@@ -114,65 +200,91 @@ with tab1:
         display_poster(index_list)
 
 with tab2:
-    selected_mood = st.selectbox('How\'s your mood?', list(
-        mood_dict.keys()), label_visibility='collapsed')
-    genre = mood_dict[selected_mood]
+    selected_emotion = st.selectbox('What kind of movie would you like to watch', [
+        'Anger', 'Anticipation', 'Disgust', 'Fear', 'Joy', 'Sadness', 'Surprise', 'Trust'], label_visibility='collapsed')
     adult = False
     if(st.radio('Are you an adult?', ['Yes', 'No'], key=3) == 'Yes'):
         adult = True
     if st.button('Recommend', key=4):
-        index_list = recommend_from_mood(genre, adult)
+        index_list = recommend_from_mood(selected_emotion.lower(), adult)
         display_poster(index_list)
 
 with tab3:
-    details = {}
+    if 'count' not in st.session_state:
+        st.session_state['count'] = 0
+        if 'check_placeholder' not in st.session_state:
+            st.session_state['check_placeholder'] = [False]*6
+        st.experimental_rerun()
+    count = st.session_state['count']
+    check_placeholder = st.session_state['check_placeholder']
+    placeholder = []
+    details = {'genres': [], 'release_year': (1950, 2019), 'rating': (
+        0.00, 10.00), 'runtime': (0, 330), 'cast': [], 'director': [], 'writers': []}
     selected_movie_genre = st.multiselect(
         'Enter movie genre', generate_set('genres'))
     details['genres'] = selected_movie_genre
-    # min_revenue = st.number_input(
-    #     'Enter the minimum revenue(in $)', key=7, min_value=0)
-    # details['revenue'] = min_revenue
-    # production_company = st.multiselect(
-    #     'Enter production company name', generate_set('production_companies'))
-    # details['production_companies'] = production_company
     col1, col2 = st.columns(2)
-    with col1:
-        start_release_year = st.number_input(
-            'Enter the minimum release year', key=8, min_value=1950, max_value=2019, value=1950)
-    with col2:
-        end_release_year = st.number_input(
-            'Enter the maximum release year', key=9, min_value=1950, max_value=2019, value=2019)
-    details['release_year'] = (start_release_year, end_release_year)
-    with col1:
-        start_rating = st.number_input(
-            'Enter the minimum rating', key=10, min_value=0.0, max_value=10.0, value=0.0, step=0.1)
-    with col2:
-        end_rating = st.number_input(
-            'Enter the maximum rating', key=11, min_value=0.0, max_value=10.0, value=10.0, step=0.1)
-    details['rating'] = (start_rating, end_rating)
-    with col1:
-        start_runtime = st.number_input(
-            'Enter the minimum runtime', key=12, min_value=0, max_value=330, value=0, step=15)
-    with col2:
-        end_runtime = st.number_input(
-            'Enter the maximum runtime', key=13, min_value=0, max_value=330, value=330, step=15)
-    details['runtime'] = (start_runtime, end_runtime)
-    cast = st.multiselect(
-        'Enter movie cast', generate_set('actors'))
-    if(cast != ''):
-        details['cast'] = cast
-    director = st.selectbox(
-        'Enter movie director\'s name', ['Choose an option']+generate_set('directors'))
-    if(director == 'Choose an option'):
-        details['director'] = []
+    for i in range(7):
+        placeholder.append(st.empty())
+    if(check_placeholder[0]):
+        with placeholder[0].container():
+            with col1:
+                start_release_year = st.number_input(
+                    'Enter the minimum release year', key=8, min_value=1950, max_value=2019, value=1950)
+            with col2:
+                end_release_year = st.number_input(
+                    'Enter the maximum release year', key=9, min_value=1950, max_value=2019, value=2019)
+            details['release_year'] = (
+                start_release_year, end_release_year)
+    if(check_placeholder[1]):
+        with placeholder[1].container():
+            cast = st.multiselect(
+                'Enter movie cast', generate_set('actors'))
+            if(cast != ''):
+                details['cast'] = cast
+    if(check_placeholder[2]):
+        with placeholder[2].container():
+            with col1:
+                start_rating = st.number_input(
+                    'Enter the minimum rating', key=10, min_value=0.0, max_value=10.0, value=0.0, step=0.1)
+            with col2:
+                end_rating = st.number_input(
+                    'Enter the maximum rating', key=11, min_value=0.0, max_value=10.0, value=10.0, step=0.1)
+            details['rating'] = (start_rating, end_rating)
+    if(check_placeholder[3]):
+        with placeholder[3].container():
+            with col1:
+                start_runtime = st.number_input(
+                    'Enter the minimum runtime', key=12, min_value=0, max_value=330, value=0, step=15)
+            with col2:
+                end_runtime = st.number_input(
+                    'Enter the maximum runtime', key=13, min_value=0, max_value=330, value=330, step=15)
+            details['runtime'] = (start_runtime, end_runtime)
+    if(check_placeholder[4]):
+        with placeholder[4].container():
+            director = st.selectbox(
+                'Enter movie director\'s name', ['Choose an option']+generate_set('directors'))
+            if(director != 'Choose an option'):
+                details['director'] = list(director)
+    if(check_placeholder[5]):
+        with placeholder[5].container():
+            writers = st.multiselect(
+                'Enter movie writers\' name', generate_set('writers'))
+            details['writers'] = writers
+    if(count < 6):
+        with placeholder[6].container():
+            if st.button("Add more filters"):
+                st.session_state['count'] = count+1
+                check_placeholder[count] = True
+                st.session_state['check_placeholder'] = check_placeholder
+    if(details['rating'][0] > details['rating'][1] or details['release_year'][0] > details['release_year'][1] or details['runtime'][0] > details['runtime'][1]):
+        st.markdown(
+            f'<p style="color:red">Please Enter valid movie details</p>', unsafe_allow_html=True)
     else:
-        details['director'] = list(director)
-    writers = st.multiselect(
-        'Enter movie writers\' name', generate_set('writers'))
-    details['writers'] = writers
-    if st.button('Recommend', key=5):
-        index_list = recommend_from_details(details)
-        if(len(index_list)):
-            display_poster(index_list)
-        else:
-            st.text("No results match your search")
+        if st.button('Recommend', key=5):
+            index_list = recommend_from_details(details)
+            if(len(index_list)):
+                display_poster(index_list)
+            else:
+                st.text("No results match your search")
+
